c6cba5d1f0fc1322cd3192757416f8350a9e4374
Rishav Bharti <rishavbharti21@gmail.com>
2022-06-18 16:37:23 +0530

    Created Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='fixed border-b-2 border-solid border-grey py-5 px-14 md:px-40 lg:px-80 w-full'>
+      <h1 className='font-semibold text-2xl'>GrowwGram</h1>
+    </div>
+  );
+};
+
+export default Navbar;
