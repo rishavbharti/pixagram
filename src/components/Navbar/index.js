13336daebe6fb9a14c3c854e011b28b2d8273519
Rishav Bharti <rishavbharti21@gmail.com>
2022-06-19 18:28:32 +0530

    Navigate to home page on click of brand name in navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='sticky border-b-2 border-solid border-gray-200 py-5 px-14 md:px-40 lg:px-80 w-full'>
-      <h1 className='font-semibold text-2xl'>GrowwGram</h1>
+      <Link to='/'>
+        <h1 className='font-semibold text-2xl'>GrowwGram</h1>
+      </Link>
     </div>
   );
 };
