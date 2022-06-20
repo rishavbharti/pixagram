c938339521d8190380db54ea21be5a206a3806c4
Rishav Bharti <rishavbharti21@gmail.com>
2022-06-20 22:47:08 +0530

    Fix typo in `REACT_APP_CACHE_DURATION`

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,4 +3,4 @@ export const FEED_CACHE_NAME = 'growwgram_feed';
 export const CACHED_TIME_HEADER = 'cached-time';
 
 // Duration in seconds for which the response should be cached
-export const CACHE_DURATION = process.env._REACT_APP_CACHE_DURATION || 60;
+export const CACHE_DURATION = process.env.REACT_APP_CACHE_DURATION || 120;
