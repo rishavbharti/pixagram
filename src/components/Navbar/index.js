349eed37c9710027be8d1939f38b42c702825cfe
Rishav Bharti <rishavbharti21@gmail.com>
2022-06-21 11:38:51 +0530

    Create separate component for theme switching. Persist theme across page refresh.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,37 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-import DarkModeIcon from '.././../assets/icons/dark_mode.svg';
-import LightModeIcon from '.././../assets/icons/light_mode.svg';
+import ToggleTheme from '../ToggleTheme';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(localStorage.theme === 'dark');
-
-  const handleClick = () => {
-    if (darkMode) {
-      setDarkMode(false);
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
-    } else {
-      setDarkMode(true);
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
-    }
-  };
-
   return (
     <div className='dark:bg-dm-navbarBg border-b-2 border-solid border-borderColor dark:border-dm-borderColor py-5 px-14 md:px-40 lg:px-80 w-full flex justify-between'>
       <Link to='/'>
         <h1 className='font-semibold text-2xl'>GrowwGram</h1>
       </Link>
 
-      <button onClick={handleClick}>
-        <img
-          src={darkMode ? LightModeIcon : DarkModeIcon}
-          alt={`Change theme to ${darkMode ? 'light' : 'dark'} mode`}
-          className='w-10 h-10 p-2 border rounded-md'
-        />
-      </button>
+      <ToggleTheme />
     </div>
   );
 };
---
+++ src/components/ToggleTheme/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+import DarkModeIcon from '.././../assets/icons/dark_mode.svg';
+import LightModeIcon from '.././../assets/icons/light_mode.svg';
+
+const ToggleTheme = () => {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem('theme') === 'dark'
+  );
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    }
+  }, [darkMode]);
+
+  const handleClick = () => {
+    if (darkMode) {
+      setDarkMode(false);
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      setDarkMode(true);
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  };
+
+  return (
+    <button onClick={handleClick}>
+      <img
+        src={darkMode ? LightModeIcon : DarkModeIcon}
+        alt={`Change theme to ${darkMode ? 'light' : 'dark'} mode`}
+        className='w-10 h-10 p-2 border rounded-md'
+      />
+    </button>
+  );
+};
+
+export default ToggleTheme;
