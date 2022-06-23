86997ab62b3a9cb495ae6a1fb1b0a4006acf1f34
Rishav Bharti <rishavbharti21@gmail.com>
2022-06-23 11:30:28 +0530

    Add favicon and logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 
 import ToggleTheme from 'components/ToggleTheme';
 
+import Logo from 'assets/logo.png';
+
 const Navbar = () => {
   return (
     <div className='dark:bg-dm-navbarBg border-b-2 border-solid border-borderColor dark:border-dm-borderColor py-5 px-14 md:px-40 lg:px-80 w-full flex justify-between'>
-      <Link to='/'>
+      <Link to='/' className='flex items-center gap-4'>
+        <img src={Logo} alt='GrowwGram' className='h-10' />
         <h1 className='font-semibold text-2xl'>GrowwGram</h1>
       </Link>
 
